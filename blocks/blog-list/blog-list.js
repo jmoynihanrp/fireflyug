b51d3d1c87e89ebccd68f9eb0a6aaccf46889466
jmoynihanrp <47532985+jmoynihanrp@users.noreply.github.com>
2026-03-24 00:17:27 -0400

    Update blog-list.js

--- blocks/blog-list/blog-list.js
+++ blocks/blog-list/blog-list.js
@@ -1,26 +1,32 @@
-export default function decorate(block) {
-  const ul = [...block.firstElementChild];
-  block.classList.add('blog-list');
-  ul.classList.add('blog-list');
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
+export default function decorateBlogList(block) {
   // setup image columns
-  [...block.children].forEach((row) => {
-    row.classList.add('blog-list-item');
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('img');
-      pic.classList.add('blog-image');
-      const title = col.querySelector('p:first-child');
+  [...block.children].forEach((ul) => {
+    ul.classList.add('blog-list-row');
+    [...ul.children].forEach((li) => {
+      li.classList.add('blog-list-item');
+
+      const img = li.querySelector('picture > img').closest('picture');
+      const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+      optimizedPic.classList.add('blog-image');
+      img.closest('picture').replaceWith(optimizedPic);
+      const title = li.querySelector('h1, h2, h3, h4, h5, h6');
       title.classList.add('blog-title');
-      const specs = col.querySelector('p:nth-child(2)');
+      const specs = li.querySelector('div:first-child');
       specs.classList.add('blog-author-date');
-      const author = specs.querySelector('span:first-child');
+      const author = specs.querySelector('p:first-child');
       author.classList.add('blog-author');
-      const date = specs.querySelector('span:nth-child(2)');
+      const date = specs.querySelector('p:last-child');
       date.classList.add('blog-date');
-      const description = col.querySelector('p:nth-child(3)');
+      const description = li.querySelector('div:nth-child(2)');
       description.classList.add('blog-description');
-      const arrow = col.querySelector('div');
+      const arrow = li.querySelector('div:last-child');
       arrow.classList.add('blog-arrow');
     });
   });
 }
+const els = document.querySelectorAll('.blog-list');
+els.forEach((el) => {
+  decorateBlogList(el);
+});
