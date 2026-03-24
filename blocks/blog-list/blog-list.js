e3d1a8e9a8dcda89c77a379858831070b57c38ee
jmoynihanrp <47532985+jmoynihanrp@users.noreply.github.com>
2026-03-24 10:10:32 -0400

    More Changes for bloglist

--- blocks/blog-list/blog-list.js
+++ blocks/blog-list/blog-list.js
@@ -1,32 +1,55 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+export default function decorate(block) {
+  const ul = document.createElement('ul');
+  ul.classList.add('blog-list-row');
 
-export default function decorateBlogList(block) {
-  // setup image columns
-  [...block.children].forEach((ul) => {
-    ul.classList.add('blog-list-row');
-    [...ul.children].forEach((li) => {
-      li.classList.add('blog-list-item');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    const firstchild = row.firstElementChild;
+    const lastchild = row.lastElementChild;
+    li.classList.add('blog-item');
 
-      const img = li.querySelector('picture > img').closest('picture');
-      const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-      optimizedPic.classList.add('blog-image');
-      img.closest('picture').replaceWith(optimizedPic);
-      const title = li.querySelector('h1, h2, h3, h4, h5, h6');
-      title.classList.add('blog-title');
-      const specs = li.querySelector('div:first-child');
-      specs.classList.add('blog-author-date');
-      const author = specs.querySelector('p:first-child');
-      author.classList.add('blog-author');
-      const date = specs.querySelector('p:last-child');
-      date.classList.add('blog-date');
-      const description = li.querySelector('div:nth-child(2)');
-      description.classList.add('blog-description');
-      const arrow = li.querySelector('div:last-child');
-      arrow.classList.add('blog-arrow');
-    });
+    const anchor = firstchild.firstElementChild;
+    const href = anchor ? anchor.textContent : '';
+    const anchor2 = document.createElement('a');
+    if (href) {
+      anchor2.href = href;
+    }
+    const img = firstchild.lastElementChild ? firstchild.lastElementChild.textContent : '';
+    const optimizedPic = document.createElement('img');
+    optimizedPic.src = img !== '' ? img : '';
+    optimizedPic.classList.add('blog-image');
+    anchor2.appendChild(optimizedPic);
+    const title = lastchild.querySelector('h2');
+    const title2 = document.createElement('p');
+    title2.textContent = title && title.textContent ? title.textContent : '';
+    title2.classList.add('blog-title');
+    anchor2.appendChild(title2);
+    const specs = lastchild.querySelectorAll('div p');
+    const authorLine = specs[0];
+    const dateLine = specs[1];
+    const authordateparagraph = document.createElement('p');
+    authordateparagraph.classList.add('blog-author-date');
+    const authorspan = document.createElement('span');
+    authorspan.classList.add('blog-author');
+    authorspan.textContent = authorLine ? authorLine.textContent : '';
+    authordateparagraph.appendChild(authorspan);
+    const datespan = document.createElement('span');
+    datespan.classList.add('blog-date');
+    datespan.textContent = dateLine && dateLine.textContent ? dateLine.textContent : '';
+    authordateparagraph.appendChild(datespan);
+    anchor2.appendChild(authordateparagraph);
+    const summaryspan = document.createElement('span');
+    const summaryparagraph = document.createElement('p');
+    summaryparagraph.classList.add('blog-summary');
+    summaryparagraph.textContent = specs[2] ? specs[2].textContent : '';
+    summaryspan.appendChild(summaryparagraph);
+    anchor2.appendChild(summaryspan);
+
+    const arrow = document.createElement('div');
+    arrow.classList.add('blog-item-arrow');
+    anchor2.appendChild(arrow);
+    li.appendChild(anchor2);
+    ul.appendChild(li);
   });
+  block.replaceChildren(ul);
 }
-const els = document.querySelectorAll('.blog-list');
-els.forEach((el) => {
-  decorateBlogList(el);
-});
