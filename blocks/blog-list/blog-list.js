6ea8bc8dac136348713e161622ad6471eb2018e7
jmoynihanrp <47532985+jmoynihanrp@users.noreply.github.com>
2026-03-24 00:08:54 -0400

    Blog list styled
    Blog list styled

--- blocks/blog-list/blog-list.js
+++ blocks/blog-list/blog-list.js
@@ -1,3 +1,26 @@
-export default async function decorate(block) {
-  block.innerHTML = 'Loading articles';
+export default function decorate(block) {
+  const ul = [...block.firstElementChild];
+  block.classList.add('blog-list');
+  ul.classList.add('blog-list');
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    row.classList.add('blog-list-item');
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('img');
+      pic.classList.add('blog-image');
+      const title = col.querySelector('p:first-child');
+      title.classList.add('blog-title');
+      const specs = col.querySelector('p:nth-child(2)');
+      specs.classList.add('blog-author-date');
+      const author = specs.querySelector('span:first-child');
+      author.classList.add('blog-author');
+      const date = specs.querySelector('span:nth-child(2)');
+      date.classList.add('blog-date');
+      const description = col.querySelector('p:nth-child(3)');
+      description.classList.add('blog-description');
+      const arrow = col.querySelector('div');
+      arrow.classList.add('blog-arrow');
+    });
+  });
 }
